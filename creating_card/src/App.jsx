7bdd4ddd94fd58c334d25376a2dfaf580da78373
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-06-08 00:48:20 +0530

    Card created

--- creating_card/src/App.jsx
+++ creating_card/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Card from './Components/Card';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>HELLO</h1>
+      <Card />
     </>
   )
 }
